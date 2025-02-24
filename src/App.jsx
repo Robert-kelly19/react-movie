00bed3f0a747ac0fbf7117102e8d5510fce86f4d
Robert Kelly <kellyhandy31@gmail.com>
2025-02-24 11:56:28 +0100

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-
-import './App.css'
-
+import { BrowserRouter, Routes, Route } from "react-router";
+import Home from "./pages/home"
+import Search from "./pages/Search"
+import Detail from "./pages/Detail"
+import Favourites from "./pages/Favourites"
+import "./App.css"
 function App() {
-
+  
   return (
-    <>
-    </>
+
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/detail/:id" element={<Detail />} />
+      <Route path="/favourite" element={<Favourites/>} />
+      <Route path="/search/:query" element={<Search />} />
+    </Routes>
+  </BrowserRouter>
+
   )
 }
 
